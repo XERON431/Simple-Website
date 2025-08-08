9075348876015ae5aed7bbc7ea6680a1ff2359b9
Siddhant Goyal <siddhantgoyal.iitg@gmail.com>
2025-08-08 11:41:51 +0530

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>About Us</h1>
+      <p>This page gives information about our site.</p>
+    </main>
+  );
+}
