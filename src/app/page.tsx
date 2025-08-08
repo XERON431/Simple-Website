cf308ba47148b87483dd62fd4173f309364faf0d
Siddhant Goyal <siddhantgoyal.iitg@gmail.com>
2025-08-08 11:52:20 +0530

    Checking auto deployed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Welcome to My Simple Website to begin with</h1>
+      <h1>Welcome to My Simple Website to begin with me</h1>
       <p>This is a basic website made with Next.js!</p>
     </main>
   );
