0120c5d6c09398af4302f984bee53b407df678c6
Siddhant Goyal <siddhantgoyal.iitg@gmail.com>
2025-08-08 12:19:06 +0530

    Navbar text color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="bg-white shadow p-4 flex justify-between">
       <h1 className="text-xl font-bold text-blue-600">CharityFund</h1>
-      <div className="space-x-4">
+      <div className="space-x-4 text-blue-600">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/campaigns">Campaigns</Link>
